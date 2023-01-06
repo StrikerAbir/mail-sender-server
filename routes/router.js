853d808aa0210e44c38f905afd3c5f9e3318ddb4
Abir Hasan <cse.170201063@gmail.com>
2023-01-06 07:15:38 +0600

    server for sending email done

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const router = new express.Router();
+const nodemailer = require("nodemailer")
+
+
 
 router.post('/send', async (req, res) => {
-    const email=req.body
+    const {email}=req.body
     console.log(email);
-    res.send(email)
+    
+    try {
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: process.env.USER,
+            pass: process.env.PASS,
+          },
+        });
+        const sub = ['Hello World', "Hello this is Ghost", "Hello this is Owner"];
+        const msg = ['it was me', 'sent successfully', 'no more today']
+        
+        const mailOptins = {
+          form: process.env.USER,
+          to: email,
+          subject: sub[Math.floor(Math.random() * sub.length)],
+          html: `<p>${msg[Math.floor(Math.random() * msg.length)]}</p>`,
+        };
+
+        transporter.sendMail(mailOptins, (error, info) => {
+            if (error) {
+                console.log('Error',error);
+            } else {
+                console.log('Email sent' + info.response);
+                res.status(201).json({status:201,info})
+            }
+        })
+
+
+    } catch (error) {
+        res.status(401).json({status:401,error})
+    }
+
 })
 
 module.exports = router;
